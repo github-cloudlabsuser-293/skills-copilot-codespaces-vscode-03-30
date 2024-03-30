5debea5361d40d565e58875f950dcebaad761d78
github-cloudlabsuser-293 <150432516+github-cloudlabsuser-293@users.noreply.github.com>
2024-03-30 15:02:31 +0000

    Copilot first commit

---
+++ skills.ts
@@ -0,0 +1,14 @@
+function calculateNumbers(var1: number, var2: number) {
+    return var1 + var2
+}
+
+function multipleNumbers(var1: number, var2: number) {
+    return var1 * var2
+}
+
+function devideNumbers(var1: number, var2: number) {
+    return var1 / var2
+}
+
+devideNumbers(10, 2) // 5
+
